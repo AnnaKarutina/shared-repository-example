d4e79116b29811c57f538c1ded0376bb5e68eaaa
Anna Karutina <anna.karutina@voco.ee>
2023-03-24 13:19:46 +0200

    Koosta Õpilane klass ja loo 2 Õpilane tüüpi objekti

---
+++ ekool.py
@@ -0,0 +1,4 @@
+from õpilane import Õpilane
+
+kati = Õpilane('Kati', 8)
+mati = Õpilane('Mati', 8)
---
+++ õpilane.py
@@ -0,0 +1,6 @@
+class Õpilane:
+    def __init__(self, nimi, klass):
+        self.nimi = nimi
+        self.klass = klass
+        self.keskmine_hinne = 0.0
+        self.ained = []
